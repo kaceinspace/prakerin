537837ad9fed5f82afdf79fb4293f19b0e0d89e3
candra <candraherdiansyah14@gmail.com>
2025-06-19 11:32:29 +0700

    crud array

---
+++ resources/views/siswa/create.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>Tambah Siswa</h2>
+    <hr>
+    <form action="/siswa" method="post">
+        @csrf
+        <select name="kelas" id="" required>
+            <option>Pilih Kelas</option>
+            <option value="XI RPL 1">XI RPL 1</option>
+            <option value="XI RPL 2">XI RPL 2</option>
+            <option value="XI RPL 3">XI RPL 3</option>
+        </select>
+        <br>
+
+        <input type="text" name="nama" placeholder="Masukan Nama" required>
+        <br>
+
+        <button type="submit">Simpan</button>
+        <button type="reset">Reset</button>
+    </form>
+    <a href="/siswa">Kembali</a>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -39,3 +39,20 @@ Route::get('promo/{barang?}/{kode?}', function ($a = null, $b = null) {
     return view('promo', compact('a', 'b'));
 
 });
+
+// Route Siswa
+use Illuminate\Support\Facades\Route;
+
+Route::get('siswa', [MyController::class, 'index']);
+// create
+Route::get('siswa/create', [MyController::class, 'create']);
+Route::post('/siswa', [MyController::class, 'store']);
+// show
+Route::get('siswa/{id}', [MyController::class, 'show']);
+
+// edit data
+Route::get('siswa/{id}/edit', [MyController::class, 'edit']);
+Route::put('siswa/{id}', [MyController::class, 'update']);
+
+// delete
+Route::delete('siswa/{id}', [MyController::class, 'destroy']);
